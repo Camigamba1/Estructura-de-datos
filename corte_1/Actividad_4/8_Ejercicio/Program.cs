925d3391794ef21eb02833c342fe2486f0edb80e
Camigamba1 <wcgamba@unicompensar.edu.co>
2021-09-08 01:10:03 -0500

    Actividades y ejercicios

---
+++ corte_1/Actividad_4/8_Ejercicio/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PUNTO_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int crow,level,more,little,height,high = 0;
+           
+            Console.Write("Ingrese el numero de personas ");
+            crow = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 1;  i <= crow; i++)
+                {
+                  Console.Write("Ingrese la altura");
+                  height = int.Parse(Console.ReadLine());
+                  more += height;
+                  level = more/crow;
+    
+                   if (height < level)
+                       {
+                          little = little + 1;
+                       }
+                    if ( height > level)
+                    {
+                          high = high +1;
+                    }
+                }
+            Console.WriteLine("Cantidad de gente con la altura mas baja que el promedio:");
+            Console.WriteLine(high);
+            Console.WriteLine("Cantidad de gente con la altura mas alta que el promedio:");
+            Console.WriteLine(little);
+        }
+    }
+}
