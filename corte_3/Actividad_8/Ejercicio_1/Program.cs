57c5193139a1d0d1f1fcf7d2330f5e637fc43f8e
Camigamba1 <wcgamba@unicompensar.edu.co>
2021-11-02 22:29:30 -0500

    se sube el ejercicio

---
+++ corte_3/Actividad_8/Ejercicio_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            
+            Console.WriteLine("El metodo WritheLine() sirve para mostrar texto en la consola, en el cual puede contener ");
+            Console.WriteLine("letras y numeros (1,2,3) en su descripcion. Como este mensaje que estas leyendo  :)   ");
+            Console.WriteLine();
+            Console.WriteLine("Oprime enter en tu teclado para continuar" );
+
+            Console.ReadLine();
+            Console.WriteLine( );
+
+            Console.WriteLine("Tambien es muy util para dejar espacios entre textos y para la comunicacion entre la consola y el usuario. ");
+            Console.WriteLine( );
+            Console.WriteLine("El metodo mas usado para este fin, por lo general es WritheLine(), pero algunas situaciones ");
+           Console.WriteLine("es posible usar el matod Writhe() sin el Line. ");
+           Console.WriteLine( );
+           Console.WriteLine( );
+        }
+
+    }
+}
